f831e368cd341a6db2aae9c6824ad864229a60b2
July Z. Paico Calderón <u20211d760@upc.edu.pe>
2026-06-12 11:30:00 -0500

    feat(options): add HotelCategoryDto for remote data source

---
+++ app/src/main/java/com/smartstay/application_mobile_frontend/feature/options/data/remote/dto/HotelCategoryDto.kt
@@ -0,0 +1,7 @@
+package com.smartstay.application_mobile_frontend.feature.options.data.remote.dto
+
+data class HotelCategoryDto(
+    val id: Int,
+    val name: String,
+    val description: String
+)
