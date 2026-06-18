f467e327630550de7e2027ee40c25c5c9be715ba
u20211d760 <u20211d760@upc.edu.pe>
2026-06-18 07:36:42 -0500

    fix(accommodation): update Hotel model to allow nullability in optional fields

--- app/src/main/java/com/smartstay/application_mobile_frontend/feature/accommodation/domain/model/Hotel.kt
+++ app/src/main/java/com/smartstay/application_mobile_frontend/feature/accommodation/domain/model/Hotel.kt
@@ -4,13 +4,12 @@ data class Hotel(
     val id: Int,
     val hostId: Int,
     val name: String,
-    val address: String,
-    val city: String,
-    val country: String,
-    val imageUrl: String,
-    val description: String,
-    val type: String,
+    val address: String?,
+    val city: String?,
+    val country: String?,
+    val imageUrl: String?,
+    val description: String?,
+    val type: String?,
     val amenities: List<String>,
     val lowestPrice: Double
 )
-
